d2c51d555c388a85407cbc60b91c21b0ff4c37b4
Eyal M <eyalmaymon007@gmail.com>
2021-06-27 16:15:47 +0300

    Add custom objects to Carteav\External.
    - KartSim has had its y rotation zeroed.
    - Created ImuSensor in Carteav\External from previous version prefab.
    - MyMap now has a copy inside Carteav\External as well.
    - All three can be added to external folder in assets folder when forking the repository.

--- Assets/Carteav/Scripts/CarteavLoader.cs
+++ Assets/Carteav/Scripts/CarteavLoader.cs
@@ -7,36 +7,7 @@ public class CarteavLoader : MonoBehaviour
     public string Address { get; private set; }
     void Start()
     {
-        if (ConnectionManager.Status == ConnectionManager.ConnectionStatus.Online))
-            try
-            {
-                string host = "localhost";
-                string port = "8080";
-                Address = $"http://{host}:{port}";
-
-                var config = new HostConfiguration {RewriteLocalhost = Config.WebHost == "*"};
-
-                Server = new NancyHost(new UnityBootstrapper(), config, new Uri(Address));
-                if (!string.IsNullOrEmpty(Config.Username))
-                {
-                    LoginAsync();
-                }
-                else
-                {
-                    Server.Start();
-                }
-            }
-            catch (SocketException ex)
-            {
-                Debug.LogException(ex);
-#if UNITY_EDITOR
-                UnityEditor.EditorApplication.isPlaying = false;
-#else
-                    // return non-zero exit code
-                    Application.Quit(1);
-#endif
-                return;
-            }
+        
     }
 
     // Update is called once per frame
