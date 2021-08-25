6f0f9296ecbe91f20ff342fe35fbf1acff828677
Eyal M <eyalmaymon007@gmail.com>
2021-08-25 13:54:24 +0300

    Add boundaries visualization and first attempt at in-boundary checking
    - DataVisualizer class for visualizing boundaries and paths.
    - Basic algorithm for inside-polygon checking in control sensor.

--- Assets/Carteav/Scripts/CarteavControlSensor.cs
+++ Assets/Carteav/Scripts/CarteavControlSensor.cs
@@ -19,17 +19,22 @@ namespace Carteav
     public enum CartState
     {
         Inactive,
-        FollowPath
+        FollowPath,
+        BoundaryCheck
     }
 
     [SensorType("Control", new[] { typeof(CartPath) })]
     public class CarteavControlSensor : SensorBase
     {
-        protected Subscriber<CartPath> Subscribe;
+        protected Subscriber<CartPath> PathSubscribe;
+        protected Subscriber<SiteBoundaries> BoundariesSubscribe;
         protected BridgeInstance Bridge;
         protected SimcartInput CartInput;
-        
-        
+
+        [SerializeField] private string PathTopic;
+        [SerializeField] private string BoundriesTopic;
+      
+
         private CartState state;
         private CartPath path;
         private int currentPointIndex;
@@ -39,35 +44,68 @@ namespace Carteav
         private float MaxAcceleration = 20f;
         private float PointReachRange = 2f;
         private DataVisualizer visualizer;
-        
-        
+        private SiteBoundaries boundaries;
+
         public void Update()
         {
             switch (state)
             {
+                case CartState.BoundaryCheck:
+                    CheckInBoundaries();
+                    break;
                 case CartState.FollowPath:
-                    FollowPathUpdate();
+                    FollowPathTick();
                     break;
             }
+        }
+        
+        private bool CheckInBoundaries()
+        {
+            Polygon mainBoundryPolygon = boundaries.MultiPolygons[0].Polygons[0];
+            Vector3 position = transform.position;
+            if (position.IsInPolygon(mainBoundryPolygon.Points))
+            {
+                for (int i = 1; i < boundaries.MultiPolygons[0].Polygons.Count; i++)
+                {
+                    Polygon polygon = boundaries.MultiPolygons[0].Polygons[i];
+                    if (position.IsInPolygon(polygon.Points))
+                    {
+                        // Inside one of the holes in the polygon - non-permitted area
+                        Debug.Log($"Cart inside non-permitted hole area at point {position}");
+                        return false;
+                    }
+                }
+            }
+            else
+            {
+                Debug.Log($"Cart left main boundary area at point {position}");
+                return false;
+            }
+
+            return true;
         }
 
         public override void OnBridgeSetup(BridgeInstance bridge)
         {
             Bridge = bridge;
-            Subscribe = OnPathUpdate;
+            PathSubscribe = OnPathReceived;
+            BoundariesSubscribe = OnBoundariesReceived;
             CartInput = GetComponentInChildren<SimcartInput>();
             state = CartState.Inactive;
             CartInput = transform.parent.GetComponentInChildren<SimcartInput>();
             cartTransform = transform.parent;
             visualizer = FindObjectOfType<DataVisualizer>();
-            
+
             var plugin = bridge.Plugin;
 
             Ros2BridgeFactory ros2Factory = new Ros2BridgeFactory();
             ros2Factory.RegSubscriber<CartPath, Carteav.Messages.CartPath>(plugin,
-                (bridgeType) => new CartPath(bridgeType));
+                (path) => new CartPath(path));
+            ros2Factory.RegSubscriber<SiteBoundaries, Carteav.Messages.SiteBoundries>(plugin,
+                (boundries) => new SiteBoundaries(boundries));
 
-            bridge.AddSubscriber(Topic, Subscribe);
+            bridge.AddSubscriber(PathTopic, PathSubscribe);
+            bridge.AddSubscriber(BoundriesTopic, BoundariesSubscribe);
         }
 
         public override void OnVisualize(Visualizer visualizer)
@@ -80,15 +118,30 @@ namespace Carteav
             Debug.Log("Visualize toggle control sensor");
         }
 
-        public void OnPathUpdate(CartPath path)
+        public void OnBoundariesReceived(SiteBoundaries boundaries)
+        {
+            this.boundaries = boundaries;
+            Debug.Log($"Boundaries received {boundaries.MultiPolygons.Count}.");
+            if (visualizer != null)
+            {
+                visualizer.VisualizeBoundaries(boundaries, 100000f);
+            }
+        }
+
+
+        public void OnPathReceived(CartPath path)
         {
             Debug.Log($"Received path {path.PathId}");
-            
+
             //FollowPath(path);
-            Vector3 offset = cartTransform.position -  path.Points[0].Point;
-            offset.y = 0;
-            visualizer.VisualizePath(path, offset);
+            if (visualizer != null)
+            {
+                Vector3 offset = cartTransform.position - path.Points[0].Point;
+                offset.y = 0;
+                visualizer.VisualizePath(path, offset);
+            }
         }
+
 
         public void FollowPath(CartPath path)
         {
@@ -97,13 +150,16 @@ namespace Carteav
                 Debug.LogError($"Path points received are either missing or too few to follow.");
                 return;
             }
-            
+
             state = CartState.FollowPath;
             currentPointIndex = 0;
             this.path = path;
         }
 
-        private void FollowPathUpdate()
+
+        
+
+        private void FollowPathTick()
         {
             Vector3 offset = path.Points[0].Point - cartTransform.position;
             offset.y = 0;
@@ -113,7 +169,7 @@ namespace Carteav
             destinationPoint.y = 0;
             cartPosition.y = 0;
             Vector3 towards = destinationPoint - cartPosition;
-            
+
             if (towards.sqrMagnitude <= PointReachRange)
             {
                 currentPointIndex++;
@@ -122,15 +178,18 @@ namespace Carteav
                     state = CartState.Inactive;
                     return;
                 }
+
                 destinationPoint = path.Points[currentPointIndex + 1].Point + offset;
                 towards = destinationPoint - cartPosition;
             }
+
             var towardsNormalized = towards.normalized;
             var cartForwards = cartTransform.forward;
             float angle = Vector3.SignedAngle(cartForwards, towardsNormalized, cartTransform.up);
             float distance = Vector3.Distance(cartPosition, destinationPoint);
             var steering = Mathf.Clamp(angle / 180f, -MaxSteering, MaxSteering);
-            var acceleration = (PointReachRange < distance ? 1 : distance / PointReachRange) * (MaxSteering - Mathf.Abs(steering)) * MaxAcceleration;
+            var acceleration = (PointReachRange < distance ? 1 : distance / PointReachRange) *
+                               (MaxSteering - Mathf.Abs(steering)) * MaxAcceleration;
             CartInput.AccelInput = acceleration;
             CartInput.SteerInput = steering;
             Debug.Log($"Position:{cartPosition}  Destination:{destinationPoint}  Angle:{angle}  " +
@@ -138,48 +197,118 @@ namespace Carteav
                       $"Distance:{distance}  " +
                       $"Index:{currentPointIndex}  Towards:{towards}  Normalized:{towardsNormalized}");
         }
-        
+    }
 
-        public class CartPath
+
+    public class CartPath
+    {
+        public List<CartPoint> Points;
+        public string PathId;
+        public uint PathLengthM;
+        public uint PathDurationSec;
+        public bool Cyclic;
+
+        public CartPath(Carteav.Messages.CartPath path)
         {
-            public CartPath(Carteav.Messages.CartPath path)
+            Points = new List<CartPoint>(path.points.ToList().ConvertAll(structPoint => new CartPoint(structPoint)));
+            PathId = path.path_id;
+            PathLengthM = path.path_length_m;
+            PathDurationSec = path.path_duration_sec;
+            Cyclic = path.cyclic;
+        }
+    }
+
+    public class CartPoint
+    {
+        public Vector3 Point;
+        public double MAXVelocityMps;
+        public double ReqVelocityMps;
+        public Int16 CurrentEtaSec;
+        public bool IsCrosswalk;
+        public bool IsJunction;
+        public bool IsSpeedBumpsgoogle;
+
+        public CartPoint(Carteav.Messages.CartPoint pointStruct)
+        {
+            var v = pointStruct.point;
+            Point = new Vector3() { x = (float)v.x, y = (float)v.z, z = (float)v.y };
+            MAXVelocityMps = pointStruct.max_velocity_mps;
+            ReqVelocityMps = pointStruct.req_velocity_mps;
+            CurrentEtaSec = pointStruct.current_eta_sec;
+            IsCrosswalk = pointStruct.is_crosswalk;
+            IsJunction = pointStruct.is_junction;
+            IsSpeedBumpsgoogle = pointStruct.is_speed_bumpsgoogle;
+        }
+    }
+
+
+    public class SiteBoundaries
+    {
+        public List<SingleSiteBoundry> MultiPolygons;
+
+        public SiteBoundaries(Carteav.Messages.SiteBoundries boundaries)
+        {
+            MultiPolygons = boundaries.multi_polygons.ToList()
+                .ConvertAll(dataBoundary => new SingleSiteBoundry(dataBoundary));
+        }
+
+        public SiteBoundaries() { }
+    }
+
+    public class SingleSiteBoundry
+    {
+        public List<Polygon> Polygons;
+        public SingleSiteBoundry(Carteav.Messages.SingleSiteBoundry boundary)
+        {
+            Polygons = boundary.polygons.ToList().ConvertAll(dataPolygon => new Polygon(dataPolygon));
+        }
+        public SingleSiteBoundry() { }
+    }
+
+    public class Polygon
+    {
+        public List<Vector3> Points;
+        public Polygon(Carteav.Messages.Polygon polygon)
+        {
+            Points = polygon.points.ToList()
+                .ConvertAll(v => new Vector3() { x = (float)v.x, y = (float)v.y, z = (float)v.z });
+        }
+
+        public Polygon() { }
+    }
+
+    public static class Extensions
+    {
+        public static bool IsInPolygon(this Vector3 testPoint, IList<Vector3> vertices)
+        {
+            if (vertices.Count < 3) return false;
+            bool isInPolygon = false;
+            var lastVertex = vertices[vertices.Count - 1];
+            foreach (var vertex in vertices)
             {
-                Points = new List<CartPoint>(path.points.ToList().ConvertAll(structPoint => new CartPoint(structPoint)));
-                PathId = path.path_id;
-                PathLengthM = path.path_length_m;
-                PathDurationSec = path.path_duration_sec;
-                Cyclic = path.cyclic;
+                if (testPoint.y.IsBetween(lastVertex.y, vertex.y))
+                {
+                    double t = (testPoint.y - lastVertex.y) / (vertex.y - lastVertex.y);
+                    double x = t * (vertex.x - lastVertex.x) + lastVertex.x;
+                    if (x >= testPoint.x) isInPolygon = !isInPolygon;
+                }
+                else
+                {
+                    if (testPoint.y == lastVertex.y && testPoint.x < lastVertex.x && vertex.y > testPoint.y)
+                        isInPolygon = !isInPolygon;
+                    if (testPoint.y == vertex.y && testPoint.x < vertex.x && lastVertex.y > testPoint.y)
+                        isInPolygon = !isInPolygon;
+                }
+
+                lastVertex = vertex;
             }
 
-            public List<CartPoint> Points;
-            public string PathId;
-            public uint PathLengthM;
-            public uint PathDurationSec;
-            public bool Cyclic;
-        }
-        
-        public class CartPoint
-        {
-            public CartPoint(Carteav.Messages.CartPoint pointStruct)
-            {
-                var v = pointStruct.point;
-                Point = new Vector3 () { x = (float)v.x, y = (float)v.z, z = (float)v.y };
-                MAXVelocityMps = pointStruct.max_velocity_mps;
-                ReqVelocityMps = pointStruct.req_velocity_mps;
-                CurrentEtaSec = pointStruct.current_eta_sec;
-                IsCrosswalk = pointStruct.is_crosswalk;
-                IsJunction = pointStruct.is_junction;
-                IsSpeedBumpsgoogle = pointStruct.is_speed_bumpsgoogle;
-            }
-            public Vector3 Point;
-            public double MAXVelocityMps;
-            public double ReqVelocityMps;
-            public Int16 CurrentEtaSec;
-            public bool IsCrosswalk;
-            public bool IsJunction;
-            public bool IsSpeedBumpsgoogle;
+            return isInPolygon;
         }
 
-       
+        public static bool IsBetween(this float x, float a, float b)
+        {
+            return (x - a) * (x - b) < 0;
+        }
     }
 }
--- Assets/Carteav/Scripts/DataVisualizer.cs
+++ Assets/Carteav/Scripts/DataVisualizer.cs
@@ -1,11 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Simulator.ScenarioEditor.Utilities;
+using Simulator.Utilities;
 using UnityEngine;
 
 namespace Carteav
 {
     public class DataVisualizer : MonoBehaviour
     {
+
+        [SerializeField] private MapBoundary boundaryPrefab;
+        [SerializeField] private MapBoundary boundaryHolePrefab;
+        [SerializeField] private Transform boundaryOrientationTransform;
+        [SerializeField] private PrefabsPools pools;
+        private List<MapBoundary> boundariesInUse = new List<MapBoundary>();
+        private void Awake()
+        {
+            var test = new SiteBoundaries()
+            {
+                MultiPolygons = new List<SingleSiteBoundry>()
+                {
+                    new SingleSiteBoundry()
+                    {
+                        Polygons = new List<Polygon>()
+                        {
+                            new Polygon()
+                            {
+                                Points = new List<Vector3>()
+                                {
+                                    new Vector3(0, 0, 0),
+                                    new Vector3(0, 1, 0),
+                                    new Vector3(1, 1, 0),
+                                    new Vector3(1, 0, 0)
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+            VisualizeBoundaries(test, 1f);
+        }
+
         public LineRenderer PathRenderer
         {
             get
@@ -29,13 +65,13 @@ namespace Carteav
                 return pathRenderer;
             }
         }
-        private LineRenderer pathRenderer;
-        [SerializeField]
-        private Material waypointsMaterial;
-        private Vector3 LineRendererPositionOffset = new Vector3(0.0f, 0.1f, 0.0f);
-        
 
-        public void VisualizePath(CarteavControlSensor.CartPath path, Vector3 offset)
+        private LineRenderer pathRenderer;
+        [SerializeField] private Material waypointsMaterial;
+        private Vector3 LineRendererPositionOffset = new Vector3(0.0f, 0.1f, 0.0f);
+
+
+        public void VisualizePath(CartPath path, Vector3 offset)
         {
             PathRenderer.positionCount = path.Points.Count + 2;
             //Debug.Log($"Setting {path.Points.Count} points");
@@ -47,18 +83,288 @@ namespace Carteav
             {
                 Vector3 point = waypointNode.Point + offset;
                 point.y = 0;
-                if (Physics.Raycast(point + raycastPositionOffset, raycastDirection, out hit, float.MaxValue, LayerMask.GetMask("Default")))
+                if (Physics.Raycast(point + raycastPositionOffset, raycastDirection, out hit, float.MaxValue,
+                    LayerMask.GetMask("Default")))
                 {
                 }
                 else
                 {
-                    Physics.Raycast(point - raycastPositionOffset, -raycastDirection, out hit, float.MaxValue, LayerMask.GetMask("Default"));
+                    Physics.Raycast(point - raycastPositionOffset, -raycastDirection, out hit, float.MaxValue,
+                        LayerMask.GetMask("Default"));
                 }
 
                 point.y = hit.point.y + LineRendererPositionOffset.y;
                 //Debug.Log($"Setting point:{point}, original:{waypointNode.Point}, hit:{hit.point}, index:{index + 1}");
                 PathRenderer.SetPosition(index + 1, point);
                 index++;
+            }
+        }
+
+        
+        
+        
+        public void VisualizeBoundaries(SiteBoundaries boundaries, float scale)
+        {
+            boundariesInUse.ForEach(boundary => pools.ReturnInstance(boundary.gameObject));
+            boundariesInUse.Clear();
+            var mainAreaPolygon = boundaries.MultiPolygons[0].Polygons[0];
+            var com = new Vector3();
+            mainAreaPolygon.Points.ForEach(p => com += p);
+            com /= mainAreaPolygon.Points.Count;
+            
+            MapBoundary mainArea = pools.GetInstance(boundaryPrefab.gameObject).GetComponent<MapBoundary>();
+            InitBoundary(mainArea, mainAreaPolygon, -com, scale);
+            var mainBoundaryTransform = mainArea.transform;
+            mainBoundaryTransform.position = boundaryOrientationTransform.position;
+            mainBoundaryTransform.localScale = boundaryOrientationTransform.localScale;
+            mainBoundaryTransform.rotation = boundaryOrientationTransform.rotation;
+            
+            List<Polygon> holes = boundaries.MultiPolygons[0].Polygons.GetRange(1, boundaries.MultiPolygons[0].Polygons.Count - 1);
+            foreach (var hole in holes)
+            {
+                var holeObj = pools.GetInstance(boundaryHolePrefab.gameObject).GetComponent<MapBoundary>();
+                InitBoundary(holeObj, hole, -com, scale);
+                var holeTransform = holeObj.transform;
+                holeTransform.parent = mainBoundaryTransform;
+                holeTransform.localPosition = new Vector3(0, 0, -0.01f);
+                holeTransform.localRotation = Quaternion.identity;
+                holeTransform.localScale = Vector3.one;
+            }
+            
+        }
+
+        private void InitBoundary(MapBoundary boundary, Polygon polygon, Vector3 offset, float scale)
+        {
+            var points3d = polygon.Points.ConvertAll(vec3 => (vec3 + offset) * scale);
+            boundary.PolygonCollider.points = points3d.ConvertAll(vec3 => (Vector2)vec3).ToArray();
+            var mesh = CreatePolygonMesh(points3d);
+            boundary.MeshFilter.mesh = mesh;
+            boundary.BoundaryOffset = offset;
+            boundariesInUse.Add(boundary);
+        }
+        
+        private static Mesh CreatePolygonMesh(List<Vector3> points3d)
+        {
+            Mesh mesh = new Mesh(); //CreateMesh(points.ConvertAll(vec3 => (vec3 - com) * scale),0);
+            //polygonCollider.CreateMesh(true, true);
+            //mesh.vertices = points.ConvertAll(vec3 => (vec3-com)*scale).ToArray();
+            Triangulator triangulator = new Triangulator(points3d.ConvertAll(vec3 => (Vector2)vec3).ToArray());
+            mesh.vertices = points3d.ToArray();
+            mesh.triangles = triangulator.Triangulate().Reverse().ToArray();
+            mesh.RecalculateNormals();
+            mesh.RecalculateBounds();
+            return mesh;
+            
+        }
+
+        Mesh CreateMeshOld(List<Vector3> nodePositions, int num)
+        {
+            int x; //Counter
+
+            //Create a new mesh
+            var mesh = new Mesh();
+
+            //Vertices
+            var vertex = new Vector3[nodePositions.Count];
+
+            for (x = 0; x < nodePositions.Count; x++)
+            {
+                vertex[x] = nodePositions[x];
+            }
+
+            //UVs
+            var uvs = new Vector2[vertex.Length];
+
+            for (x = 0; x < vertex.Length; x++)
+            {
+                if ((x % 2) == 0)
+                {
+                    uvs[x] = new Vector2(0, 0);
+                }
+                else
+                {
+                    uvs[x] = new Vector2(1, 1);
+                }
+            }
+
+            //Triangles
+            var tris = new int[3 * (vertex.Length - 2)]; //3 verts per triangle * num triangles
+            int C1, C2, C3;
+
+            if (num == 0)
+            {
+                C1 = 0;
+                C2 = 1;
+                C3 = 2;
+
+                for (x = 0; x < tris.Length; x += 3)
+                {
+                    tris[x] = C1;
+                    tris[x + 1] = C2;
+                    tris[x + 2] = C3;
+
+                    C2++;
+                    C3++;
+                }
+            }
+            else
+            {
+                C1 = 0;
+                C2 = vertex.Length - 1;
+                C3 = vertex.Length - 2;
+
+                for (x = 0; x < tris.Length; x += 3)
+                {
+                    tris[x] = C1;
+                    tris[x + 1] = C2;
+                    tris[x + 2] = C3;
+
+                    C2--;
+                    C3--;
+                }
+            }
+
+            //Assign data to mesh
+            mesh.vertices = vertex;
+            mesh.uv = uvs;
+            mesh.triangles = tris;
+
+            //Recalculations
+            mesh.RecalculateNormals();
+            mesh.RecalculateBounds();
+            // mesh.Optimize();
+
+            //Name the mesh
+            mesh.name = "MyMesh";
+
+            //Return the mesh
+            return mesh;
+        }
+
+
+        public class Triangulator
+        {
+            private List<Vector2> m_points = new List<Vector2>();
+
+            public Triangulator(Vector2[] points)
+            {
+                m_points = new List<Vector2>(points);
+            }
+
+            public int[] Triangulate()
+            {
+                List<int> indices = new List<int>();
+
+                int n = m_points.Count;
+                if (n < 3)
+                    return indices.ToArray();
+
+                int[] V = new int[n];
+                if (Area() > 0)
+                {
+                    for (int v = 0; v < n; v++)
+                        V[v] = v;
+                }
+                else
+                {
+                    for (int v = 0; v < n; v++)
+                        V[v] = (n - 1) - v;
+                }
+
+                int nv = n;
+                int count = 2 * nv;
+                for (int v = nv - 1; nv > 2;)
+                {
+                    if ((count--) <= 0)
+                        return indices.ToArray();
+
+                    int u = v;
+                    if (nv <= u)
+                        u = 0;
+                    v = u + 1;
+                    if (nv <= v)
+                        v = 0;
+                    int w = v + 1;
+                    if (nv <= w)
+                        w = 0;
+
+                    if (Snip(u, v, w, nv, V))
+                    {
+                        int a, b, c, s, t;
+                        a = V[u];
+                        b = V[v];
+                        c = V[w];
+                        indices.Add(a);
+                        indices.Add(b);
+                        indices.Add(c);
+                        for (s = v, t = v + 1; t < nv; s++, t++)
+                            V[s] = V[t];
+                        nv--;
+                        count = 2 * nv;
+                    }
+                }
+
+                indices.Reverse();
+                return indices.ToArray();
+            }
+
+            private float Area()
+            {
+                int n = m_points.Count;
+                float A = 0.0f;
+                for (int p = n - 1, q = 0; q < n; p = q++)
+                {
+                    Vector2 pval = m_points[p];
+                    Vector2 qval = m_points[q];
+                    A += pval.x * qval.y - qval.x * pval.y;
+                }
+
+                return (A * 0.5f);
+            }
+
+            private bool Snip(int u, int v, int w, int n, int[] V)
+            {
+                int p;
+                Vector2 A = m_points[V[u]];
+                Vector2 B = m_points[V[v]];
+                Vector2 C = m_points[V[w]];
+                if (Mathf.Epsilon > (((B.x - A.x) * (C.y - A.y)) - ((B.y - A.y) * (C.x - A.x))))
+                    return false;
+                for (p = 0; p < n; p++)
+                {
+                    if ((p == u) || (p == v) || (p == w))
+                        continue;
+                    Vector2 P = m_points[V[p]];
+                    if (InsideTriangle(A, B, C, P))
+                        return false;
+                }
+
+                return true;
+            }
+
+            private bool InsideTriangle(Vector2 A, Vector2 B, Vector2 C, Vector2 P)
+            {
+                float ax, ay, bx, by, cx, cy, apx, apy, bpx, bpy, cpx, cpy;
+                float cCROSSap, bCROSScp, aCROSSbp;
+
+                ax = C.x - B.x;
+                ay = C.y - B.y;
+                bx = A.x - C.x;
+                by = A.y - C.y;
+                cx = B.x - A.x;
+                cy = B.y - A.y;
+                apx = P.x - A.x;
+                apy = P.y - A.y;
+                bpx = P.x - B.x;
+                bpy = P.y - B.y;
+                cpx = P.x - C.x;
+                cpy = P.y - C.y;
+
+                aCROSSbp = ax * bpy - ay * bpx;
+                cCROSSap = cx * apy - cy * apx;
+                bCROSScp = bx * cpy - by * cpx;
+
+                return ((aCROSSbp >= 0.0f) && (bCROSScp >= 0.0f) && (cCROSSap >= 0.0f));
             }
         }
     }
