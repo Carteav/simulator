828c5ffe0f9639f72f52aac8c869146f4c314fe5
Eyal M <eyalmaymon007@gmail.com>
2021-09-24 14:59:47 +0300

    Fix sensor prefabs
    - Fixes errors on startup.

--- Assets/External/Sensors/RoboBpearlLidar/RoboBpearlLidar.cs
+++ Assets/External/Sensors/RoboBpearlLidar/RoboBpearlLidar.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Simulator.Bridge.Data;
 using Simulator.Sensors;
+using Simulator.Utilities;
 using UnityEngine;
 
 namespace Simulator.Sensors
 {
+    [SensorType("Lidar", new[] { typeof(PointCloudData) })]
     public class RoboBpearlLidar : CarteavLidarSensor
     {
     }
