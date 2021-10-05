a0eb6a0e54e481175ab9b7c7044b9a1ba9a645a6
Eyal M <eyalmaymon007@gmail.com>
2021-10-05 23:19:43 +0300

    Clean unused using declarations

--- Assets/Carteav/Scripts/RadarSensorCarteav.cs
+++ Assets/Carteav/Scripts/RadarSensorCarteav.cs
@@ -5,7 +5,6 @@
  *
  */
 
-using System;
 using System.Linq;
 using System.Collections.Generic;
 using Carteav.Messages;
--- Assets/Carteav/Scripts/SimcartInput.cs
+++ Assets/Carteav/Scripts/SimcartInput.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using Carteav;
-using Carteav.Messages;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Carteav
 {
